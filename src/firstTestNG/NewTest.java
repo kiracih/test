d3ed950a157c84fed7ed461403ff55bc8bea7347
kiracih <ehalil13@gmail.com>
2017-05-23 22:20:39 +0300

    Selenium Test Project created.

---
+++ src/firstTestNG/NewTest.java
@@ -0,0 +1,79 @@
+package firstTestNG;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest {
+
+	public String baseUrl = "http://newtours.demoaut.com/";
+	String driverPath = "C:\\chromedriver.exe";
+	public WebDriver driver;
+
+	public static void infoBox(String infoMessage, String titleBar) {
+		JOptionPane.showMessageDialog(null, infoMessage, titleBar,
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	@Test
+	public void verifyHomepageTitle() {
+
+		JFrame frame = new JFrame();
+		String[] options = new String[2];
+		options[0] = new String("Başlat");
+		options[1] = new String("Vazgeç");
+		Container frameContent = frame.getContentPane();
+		frameContent.setBackground(Color.RED);
+
+		int testStartResult = JOptionPane.showOptionDialog(frameContent,
+				"verifyHomepageTitle Testini Başlatmak İstiyor Musunuz?",
+				"verifyHomepageTitle Testi", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, options, null);
+
+		if (testStartResult == 0) {
+			System.out.println("Chrome açılıyor");
+			System.setProperty("webdriver.chrome.driver", driverPath);
+			driver = new ChromeDriver();
+			driver.get(baseUrl);
+
+			String expectedTitle = "Welcome: Mercury Tours";
+			String actualTitle = driver.getTitle();
+
+			if (!actualTitle.equals(expectedTitle)) {
+
+				infoBox("verifyHomepageTitle Testi Başarısız\n\nGerçek: "
+						+ actualTitle + "\nBeklenen: " + expectedTitle,
+						"Başlık Hatalı");
+
+				System.out.println("Gerçek: " + actualTitle);
+				System.out.println("Beklenen: " + expectedTitle);
+
+				driver.close();
+
+			} else {
+
+				infoBox("verifyHomepageTitle Testi Başarılı\n\nGerçek: "
+						+ actualTitle + "\nBeklenen: " + expectedTitle,
+						"Başlık Doğru");
+
+				System.out.println("Test Başarılı");
+				driver.close();
+			}
+
+			Assert.assertEquals(actualTitle, expectedTitle);
+
+		} else {
+
+			driver.close();
+
+		}
+
+	}
+}
